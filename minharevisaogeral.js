3eb4132e7447758a18ad9aab63f8a9ca6ebce875
Jair dos santos <dossantosjair140@gmail.com>
2022-06-21 23:06:55 -0300

    minha revisao

---
+++ minharevisaogeral.js
@@ -0,0 +1,19 @@
+// revisao  variaveis;
+
+//var meuNome = "jair";
+//var sobreNome = "dos santos";
+
+//console.log("jair"+ " "+ "dos" + " "+ "santos")
+
+//criacao de variaveis, console.log como resultdo e concatena estrigs
+
+                            //erosfutios e  calbecks
+
+
+var meuNome = (par1,par2)=>par1+par2;
+
+var sobreNome = (par3,par4)=>par3+par4;
+
+var nomeCompleto = (par1,par2,endentidade)=> endentidade(par1,par2);
+
+console.log(nomeCompleto("Jair" ,  " "+ "dos"+ " "+ "santos",meuNome))
